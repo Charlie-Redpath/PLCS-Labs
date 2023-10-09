bfe0af04b68ed703a8723460fed8e5231a983ae1
csc <_@>
2023-10-09 13:51:01 +0100

    Week 1 amended

--- Week 1/woody.java
+++ Week 1/woody.java
@@ -3,14 +3,14 @@ import javax.swing.*;
 
 public class woody
 {
-    public static void main(final String[] args)
+    public static void main(String[] args)
     {
         final String input;
         input = JOptionPane.showInputDialog("n (m): ");
         final double n = Double.parseDouble(input);
 
         final int halves = (int)Math.floor(n * 2);
-        final int fiths = (int)Math.floor((n - halves / 2) * 5);
+        final int fiths = (int)Math.floor((n - (double)halves / 2) * 5);
 
         JOptionPane.showMessageDialog( null, n + " = " + halves + "×½ + " + fiths + "×⅕ + "
                 + (n - halves / 2 - fiths / 5) + 'm');
