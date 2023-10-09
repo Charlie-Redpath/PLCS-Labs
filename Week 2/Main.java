49aa273a6aad8b982b65b7444774a2b126d048b5
csc <_@>
2023-10-09 13:58:19 +0100

    Week 2 reformat

--- Week 2/Main.java
+++ Week 2/Main.java
@@ -14,19 +14,17 @@ public class Main {
     }
 }
 
-class Worm
-{
+class Worm {
     int size;
     String name;
     Color color;
 
-    Worm()
-    {
+    Worm() {
         size = 5;
         name = "Wiggly Worm";
     }
-    void print()
-    {
+
+    void print() {
         System.out.println("My name is " + name);
         System.out.println("I grow 1 cm each year and I am " + size + " Years old. Happy birthday to me.");
     }
@@ -34,18 +32,23 @@ class Worm
     void move() {
         System.out.println("move() called");
     }
+
     void grow() {
         System.out.println("grow() called");
     }
+
     void eat() {
         System.out.println("eat() called");
     }
+
     void getSize() {
         System.out.println("getSize() called");
     }
+
     void setSize() {
         System.out.println("setSize() called");
     }
+
     void start() {
         System.out.println("start() called");
     }
